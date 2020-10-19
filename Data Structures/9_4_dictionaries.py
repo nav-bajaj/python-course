1e1bbcfa8bbe2b59e183935386dc79f84a51ed6c
Navdeep Bajaj <navdeep.bajajtravels@gmail.com>
2020-10-19 18:03:39 +0530

    Dictionaries

---
+++ Data Structures/9_4_dictionaries.py
@@ -0,0 +1,28 @@
+#name = "mbox-short.txt"
+
+
+handle = open("mbox-short.txt")
+senders = list()
+
+for line in handle :
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    senders.append(words[1])
+
+
+
+
+counts = dict()
+bigsender = int()
+bigsendername = str()
+
+for max_sender in senders :
+    counts[max_sender] = counts.get(max_sender,0) + 1
+    if bigsender is None or bigsender < counts[max_sender]:
+        bigsender = counts[max_sender]
+        bigsendername = max_sender
+
+
+print(bigsendername, bigsender)
